83f97876bcaf9cbb0934dcd4de65e6556e13eb4a
Kilhwan Kim <sys4ppl@gmail.com>
2022-05-24 01:52:14 +0900

    onLoad.R: error correction for the case when localeTocharSet() returns multiple values.

--- R/onLoad.R
+++ R/onLoad.R
@@ -109,7 +109,7 @@
   assign("backupUserDic", file.path(backupUserDicPath,DicUser), .KoNLPEnv)
   assign("ScalaVer", .ScalaVer, .KoNLPEnv)
 
-  if(is.na(localeToCharset()) != TRUE) {
+  if(any(is.na(localeToCharset()))) {
     if(all((localeToCharset()[1] == c("UTF-8", "CP949", "EUC-KR")) == FALSE)){
       packageStartupMessage("This R shell doesn't contain any Hangul encoding.\nFor fully use, any of 'UTF-8', 'CP949', 'EUC-KR' needs to be used for R shell encoding.")
     }
